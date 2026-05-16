189bed385c549fb461ba5d3f8a63bb8eefed2ccf
sukranbasaran <sukranbasaran123@gmail.com>
2026-05-16 22:18:20 +0300

    test: PCA tek boyut dönüşüm testi eklendi

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -14,3 +14,15 @@ def test_pca_output_shape():
 
     assert X_train_pca.shape == (100, 1)
     assert X_test_pca.shape == (20, 1)
+
+def test_pca_output_dimension_is_one():
+    X_train = np.random.rand(50, 8)
+    X_test = np.random.rand(10, 8)
+
+    X_train_pca, X_test_pca = apply_pca(
+        X_train,
+        X_test
+    )
+
+    assert X_train_pca.shape[1] == 1
+    assert X_test_pca.shape[1] == 1
