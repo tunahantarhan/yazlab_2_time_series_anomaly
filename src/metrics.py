ce178c2ffbeeae10f4492c71b6faf5fafc136d82
sukranbasaran <sukranbasaran123@gmail.com>
2026-05-18 16:40:45 +0300

    feat: precision ve recall hesaplama eklendi

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,9 @@
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score
+)
 
 def calculate_accuracy(y_true, y_pred):
     return accuracy_score(
@@ -10,6 +13,19 @@ def calculate_accuracy(y_true, y_pred):
 
 def calculate_f1_score(y_true, y_pred):
     return f1_score(
+        y_true,
+        y_pred
+    )
+
+def calculate_precision(y_true, y_pred):
+    return precision_score(
+        y_true,
+        y_pred
+    )
+
+
+def calculate_recall(y_true, y_pred):
+    return recall_score(
         y_true,
         y_pred
     )
