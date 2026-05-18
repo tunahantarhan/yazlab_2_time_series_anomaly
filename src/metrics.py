88bbd812d429e73dc82df09da26b064ef8f97c63
sukranbasaran <sukranbasaran123@gmail.com>
2026-05-18 16:33:02 +0300

    feat: F1-score hesaplama fonksiyonu eklendi

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,15 @@
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 
 def calculate_accuracy(y_true, y_pred):
     return accuracy_score(
+        y_true,
+        y_pred
+    )
+
+def calculate_f1_score(y_true, y_pred):
+    return f1_score(
         y_true,
         y_pred
     )
