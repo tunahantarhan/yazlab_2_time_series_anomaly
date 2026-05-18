fef32c6ebe376112c23334b14bc2162fa5334d14
sukranbasaran <sukranbasaran123@gmail.com>
2026-05-18 16:43:16 +0300

    feat: confusion matrix hesaplama eklendi

--- src/metrics.py
+++ src/metrics.py
@@ -3,6 +3,7 @@ from sklearn.metrics import (
     f1_score,
     precision_score,
     recall_score
+    confusion_matrix
 )
 
 def calculate_accuracy(y_true, y_pred):
@@ -26,6 +27,12 @@ def calculate_precision(y_true, y_pred):
 
 def calculate_recall(y_true, y_pred):
     return recall_score(
+        y_true,
+        y_pred
+    )
+    
+def calculate_confusion_matrix(y_true, y_pred):
+    return confusion_matrix(
         y_true,
         y_pred
     )
