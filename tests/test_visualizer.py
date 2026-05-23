b61b0b8608a3c3afe144750e23ac0f48377a36ef
tunahantarhan <tunahantarhan99@gmail.com>
2026-05-23 20:04:44 +0300

    test(visualizer): zaman serisi üzerinde anomali noktalarını çizecek fonksiyonun birim testi eklendi (RED)

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -20,3 +20,16 @@ def test_plot_transition_heatmap(mock_show):
     visualizer.plot_transition_heatmap(transition_probs)
     # "plt.show()" fonksiyonunun en az bir kere çağrıldığı doğrulanır
     mock_show.assert_called_once()
+    
+@patch("src.visualizer.plt.show")
+def test_plot_anomalies_on_timeseries(mock_show):
+    visualizer = AutomataVisualizer()
+    
+    # mock zaman serisi
+    data = [10, 12, 11, 15, 50, 12, 13, 14, 55, 11]
+    
+    # mock anomali indeksleri
+    anomaly_indices = [4, 8]
+    
+    visualizer.plot_anomalies_on_timeseries(data, anomaly_indices)
+    mock_show.assert_called_once()
