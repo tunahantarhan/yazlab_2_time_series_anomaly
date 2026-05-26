57e58724373dae6f0703dd0c9f6150aebca14d43
sukranbasaran <sukranbasaran123@gmail.com>
2026-05-26 20:40:13 +0300

    test: inference runtime testi eklendi

---
+++ tests/test_inference_runtime.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from src.inference import measure_inference_time
+
+
+class DummyModel:
+    def predict(self, X, verbose=0):
+        return np.zeros((len(X), 1))
+
+
+def test_inference_runtime_returns_positive_value():
+    X = np.random.rand(20, 5, 1)
+
+    model = DummyModel()
+
+    runtime = measure_inference_time(
+        model,
+        X
+    )
+
+    assert runtime > 0
